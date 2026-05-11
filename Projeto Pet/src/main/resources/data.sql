f57cb8e055452ee8c77ee89c06f2885603bfae33
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-11 20:03:03 -0300

    inserindo mais dois tutores com os mesmos processamentos; id, nome,cpf,email,telefone,data_nascimento etc..

--- Projeto Pet/src/main/resources/data.sql
+++ Projeto Pet/src/main/resources/data.sql
@@ -1,3 +1,9 @@
 -- ---------- TUTORES ----------
 INSERT INTO tb_tutor (id, nome, cpf, email, telefone, data_nascimento, aceita_comunicacao, criado_em, atualizado_em)
 VALUES (1, 'Maria da Silva', '123.456.789-00', 'maria@email.com', '(11) 98765-4321', '1990-05-20', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO tb_tutor (id, nome, cpf, email, telefone, data_nascimento, aceita_comunicacao, criado_em, atualizado_em)
+VALUES (2, 'João Pereira', '987.654.321-00', 'joao@email.com', '(11) 91234-5678', '1985-08-15', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+ 
+INSERT INTO tb_tutor (id, nome, cpf, email, telefone, data_nascimento, aceita_comunicacao, criado_em, atualizado_em)
+VALUES (3, 'Ana Costa', '111.222.333-44', 'ana@email.com', '(21) 99999-0000', '1995-11-30', FALSE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
