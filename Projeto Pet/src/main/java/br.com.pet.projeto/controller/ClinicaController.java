89d3c365fd201f2df80829d3d75d32855163e843
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-12 19:23:47 -0300

    subindo os imports no clinicaController para iniciar o desenvolvimento

---
+++ Projeto Pet/src/main/java/br.com.pet.projeto/controller/ClinicaController.java
@@ -0,0 +1,16 @@
+package br.com.fiap.clyvovet.controller;
+ 
+import br.com.fiap.clyvovet.dto.request.ClinicaRequest;
+import br.com.fiap.clyvovet.dto.response.ClinicaResponse;
+import br.com.fiap.clyvovet.entity.Clinica;
+import br.com.fiap.clyvovet.service.ClinicaService;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.responses.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
