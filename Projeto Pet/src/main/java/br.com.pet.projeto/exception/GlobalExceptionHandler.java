90408e88de52ceb7ac50d5ae3a1d310e9eb99512
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-12 19:48:05 -0300

    RestControllerAdvice para capturar e formatar erros de validação e exceções de negócio em um padrão JSON único.

---
+++ Projeto Pet/src/main/java/br.com.pet.projeto/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package br.com.fiap.clyvovet.exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
