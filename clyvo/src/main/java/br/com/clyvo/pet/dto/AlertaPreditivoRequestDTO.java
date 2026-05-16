671b800eef4b47ff8c26a0fdf14ef65062d17d31
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-16 19:12:42 -0300

    ajustando o tipo na classe

--- clyvo/src/main/java/br/com/clyvo/pet/dto/AlertaPreditivoRequestDTO.java
+++ clyvo/src/main/java/br/com/clyvo/pet/dto/AlertaPreditivoRequestDTO.java
@@ -20,8 +20,9 @@ public class AlertaPreditivoRequestDTO {
     @NotNull(message = "O identificador do paciente é obrigatório")
     private Long pacienteId;
 
+    // Correção: mudado de 'type' para 'tipo'
     @NotNull(message = "O tipo de alerta preditivo é obrigatório")
-    private TipoAlertaPreditivo type;
+    private TipoAlertaPreditivo tipo;
 
     @NotBlank(message = "A mensagem de alerta é obrigatória")
     @Size(max = 500, message = "A mensagem deve conter no máximo 500 caracteres")
