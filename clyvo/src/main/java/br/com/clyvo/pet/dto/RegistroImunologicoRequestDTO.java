deecda0b9a412bddfa3d479f6337d3a9a7c8e11a
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-16 20:00:02 -0300

    Adiciona campos de nome, lote e data com validação no RequestDTO
    Insere os atributos nomeVacina, lote e dataAplicacao aplicando regras de preenchimento do Bean Validation e mensagens de erro customizadas.

--- clyvo/src/main/java/br/com/clyvo/pet/dto/RegistroImunologicoRequestDTO.java
+++ clyvo/src/main/java/br/com/clyvo/pet/dto/RegistroImunologicoRequestDTO.java
@@ -19,4 +19,14 @@ public class RegistroImunologicoRequestDTO {
 
     @NotNull(message = "O identificador do paciente é obrigatório")
     private Long pacienteId;
+
+    @NotBlank(message = "O nome do imunizante é obrigatório")
+    @Size(max = 100, message = "O nome da vacina deve conter no máximo 100 caracteres")
+    private String nomeVacina;
+
+    @Size(max = 50, message = "O lote deve conter no máximo 50 caracteres")
+    private String lote;
+
+    @NotNull(message = "A data de aplicação é obrigatória")
+    private LocalDate dataAplicacao;
 }
