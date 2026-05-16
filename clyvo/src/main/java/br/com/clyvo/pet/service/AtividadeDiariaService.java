a27c6262d174327554973fdebbfacd6927ed7c99
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-16 19:27:19 -0300

    Cria AtividadeDiariaService e define imports estruturais
    Implementa a classe de serviço com suporte ao Spring (@Service), Lombok para injeção de dependências e mapeamento de DAOs e DTOs de atividades diárias.

---
+++ clyvo/src/main/java/br/com/clyvo/pet/service/AtividadeDiariaService.java
@@ -0,0 +1,18 @@
+package br.com.clyvo.pet.service;
+
+
+
+import br.com.clyvo.pet.dto.AtividadeDiariaRequestDTO;
+import br.com.clyvo.pet.dto.AtividadeDiariaResponseDTO;
+import br.com.clyvo.pet.entity.PacienteAnimal;
+import br.com.clyvo.pet.entity.AtividadeDiaria;
+import br.com.clyvo.pet.repository.AtividadeDiariaDao;
+import br.com.clyvo.pet.repository.PacienteAnimalDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AtividadeDiariaService {
+}
