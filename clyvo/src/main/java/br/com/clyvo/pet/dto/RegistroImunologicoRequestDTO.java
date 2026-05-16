7725937112682e64332c766f69609e4d5a48f78c
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-16 19:58:52 -0300

    Cria estrutura base de RegistroImunologicoRequestDTO
    Adiciona a classe DTO de entrada para registro vacinal contendo anotações estruturais do Lombok e os imports necessários para validação de campos.

---
+++ clyvo/src/main/java/br/com/clyvo/pet/dto/RegistroImunologicoRequestDTO.java
@@ -0,0 +1,18 @@
+package br.com.clyvo.pet.dto;
+
+import br.com.clyvo.pet.enums.StatusImunizacao;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistroImunologicoRequestDTO {
