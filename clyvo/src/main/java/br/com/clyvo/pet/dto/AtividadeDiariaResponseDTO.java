fff51258f9e184969a114f914a33282e8c5a1a61
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-16 19:25:13 -0300

    Adiciona imports estruturais em AtividadeDiariaRequestDTO
    Importa o enum TipoRotina, a classe LocalDate e o pacote Lombok necessários para a estrutura da classe.

---
+++ clyvo/src/main/java/br/com/clyvo/pet/dto/AtividadeDiariaResponseDTO.java
@@ -0,0 +1,12 @@
+package br.com.clyvo.pet.dto;
+
+import br.com.clyvo.pet.enums.TipoRotina;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+public class AtividadeDiariaResponseDTO {
+}
