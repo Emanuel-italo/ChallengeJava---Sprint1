e4e4fb7dc80efadc6761a7897eb6f4c7dbbd9c34
emanuel-italo <emanuelitaloleal@hotmail.com>
2026-05-16 19:20:27 -0300

    AtividadeDiaria criando no request

---
+++ clyvo/src/main/java/br/com/clyvo/pet/dto/AtividadeDiariaRequestDTO.java
@@ -0,0 +1,14 @@
+package br.com.clyvo.pet.dto;
+
+import br.com.clyvo.pet.enums.TipoRotina;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+public class AtividadeDiariaRequestDTO {
+}
